db5d6b9e19af5a090263a267b489e684095ffb94
Asheesh Ranjan <asheeshranjan1@gmail.com>
2013-09-28 10:03:03 +0530

    Support for all arguments except CPU_LOAD_TIME added

--- notify-highload.sh
+++ notify-highload.sh
@@ -3,33 +3,33 @@
 #Generates notification when CPU load average is above specified limit
 
 #Default Settings
-AUDIO=NO #Sets audio notification
-GRAPHICAL=NO #Sets notify-send/desktop notification
+AUDIO=YES #Sets audio notification
+GRAPHICAL=YES #Sets notify-send/desktop notification
 VERBOSE=NO #Sets terminal output
 CPU_LOAD_TIME=1 #Set time duration for which to check load averages
-CPU_LOAD_LMT=90 #Set limit for high load average
-TIME_GAP_NOTIFICATIONS=0 #Set gap between successive notifications
-TIME_GAP_BOOT=30 #Set initial delay in starting
+CPU_LOAD_LMT=90 #Set limit for high load average. Any number >0
+TIME_GAP_NOTIFICATIONS=0 #Set gap between successive notifications. Any number >=0
+TIME_GAP_BOOT=30 #Set initial delay in starting. Any number >=0
 
-set -- `getopt -q agvc:l:t:i: "$@"` #Parse command line parameters and options
+#set -- `getopts agvc:l:t:i: "$@"` #Parse command line parameters and options
+#while [ -n "$1" ] #Set settings to passed parameters
 
-while [ -n "$1" ] #Set settings to passed parameters
+while getopts :agvc:l:t:i: opt
 do
-    case "$1" in
-	-a) AUDIO=YES;;
-	-g) GRAPHICAL=YES;;
-	-v) VERBOSE=YES;;
-	-c) CPU_LOAD_TIME=$2
-	    shift ;;
-	-l) CPU_LOAD_LMT=$2
-	    shift ;;
-	-t) TIME_GAP_NOTIFICATIONs=$2
-	    shift ;;
-	-i) TIME_GAP_BOOT=$2
-	    shift ;;
-	-*) ;;
+    case "$opt" in
+	a) AUDIO=YES;;
+	g) GRAPHICAL=YES;;
+	v) VERBOSE=YES;;
+	c) CPU_LOAD_TIME="$OPTARG";;
+	    #shift ;;
+	l) CPU_LOAD_LMT="$OPTARG";;
+	    #shift ;;
+	t) TIME_GAP_NOTIFICATIONs="$OPTARG";;
+	    #shift ;;
+	i) TIME_GAP_BOOT="$OPTARG";;
+	    #shift ;;
+	*) ;;
 	esac
-    shift
 done
 
 if [ "YES" == $VERBOSE ] #Displaying settings
@@ -40,7 +40,7 @@ then
     echo "Verbose : $VERBOSE"
     echo "Load Average Used : $CPU_LOAD_TIME"
     echo "Limit for high load : $CPU_LOAD_LMT"
-    echo ""
+    echo "Initial time delay : $TIME_GAP_BOOT"
 fi
 
 
@@ -54,21 +54,32 @@ do
     load=`echo "scale=2; $load * 100" | bc` #Convert to integer
     load=`printf "%.0f" $load` #Remove decimal digits
 
-    highload=$((`nproc`*90)) #Calculate high load threshold based on number of cores
+    highload=$((`nproc`*$CPU_LOAD_LMT)) #Calculate high load threshold based on number of cores
     
     if test $load -ge $highload
     then
 	#Change to default player if pulse audio is not installed
-	paplay /usr/share/sounds/ubuntu/stereo/system-ready.ogg
+	if [ "YES" == $AUDIO ] 
+	then
+	    paplay /usr/share/sounds/ubuntu/stereo/system-ready.ogg
+	fi
 
 	#Generates visual notification
-	notify-send -i /usr/share/icons/default.kde4/128x128/devices/cpu.png -t 400 "High CPU Load"'!' \
-	"The CPU has been hard at work in the past minute"
+	if [ "YES" == $GRAPHICAL ]
+	    then
+	    notify-send -i /usr/share/icons/default.kde4/128x128/devices/cpu.png -t 400 "High CPU Load"'!' \
+	fi
+	"The CPU has been hard at work in the past minute."
 	
 	#PC Speaker is disabled on default configuration of Ubuntu
 	#printf "\a" 
 	
-	sleep 5 #High load averages are reflected for the next few seconds 
+	if [ "YES" == $VERBOSE ]
+	then
+	    echo "CPU Load High!"
+	fi
+
+	sleep $TIME_GAP_NOTIFICATIONS #High load averages are reflected for the next few seconds 
 
     fi
     
