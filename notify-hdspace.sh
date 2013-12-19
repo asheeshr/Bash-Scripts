49f2377a96e07c877d43b9a7e1fd6c38551a9d63
Asheesh Ranjan <asheeshranjan1@gmail.com>
2013-12-19 14:23:03 +0530

    Added graphical desktop notification

--- notify-hdspace.sh
+++ notify-hdspace.sh
@@ -2,7 +2,10 @@
 
 #Generate notification when hard disk utilisation exceeds specified limit
 
-HD_LMT=80%
+HD_LMT=70%
+
+NOTIFICATION_SOUND="/usr/share/sounds/ubuntu/stereo/system-ready.ogg"
+NOTIFICATION_ICON="/usr/share/icons/default.kde4/128x128/devices/drive-harddisk.png"
 
 list=`df | sort -n` 
 list=($list) #Create list from string
@@ -14,8 +17,9 @@ do
     then
 	if [ ${list[i+4]} \> $HD_LMT ] #Check utilisation. > needs to be escaped
 	then
-	    paplay /usr/share/sounds/ubuntu/stereo/system-ready.ogg &
+	    disk=$(basename ${list[i]})
+	    paplay $NOTIFICATION_SOUND &
+	    notify-send -i $NOTIFICATION_ICON "Storage Memory Full!" "Disk $disk is over ${list[i+4]} full"
 	fi
     fi 
 done
-
